231a3884c2b7940f79ef537627f2fb0c9ee88a1f
Patrick Jakobsen <patrick.jakobsen@icloud.com>
2026-02-26 21:05:43 +0100

    Initial Commit

---
+++ Elbert/ElbertApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElbertApp.swift
+//  Elbert
+//
+//  Created by Patrick Jakobsen on 26/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ElbertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
